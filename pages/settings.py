368c2300efbb63d139c6a0de1ddfa5a5255915d6
Katastofa <keynalastor@gmail.com>
2024-12-22 23:01:01 +0500

    кайф

---
+++ pages/settings.py
@@ -0,0 +1,12 @@
+import flet as ft
+
+def settings_content(page):
+    return ft.Column(
+        controls=[
+            ft.Text("Это главная страница", size=24),
+            ft.Text("Добро пожаловать в приложение!", size=16),
+            ft.Image(src="https://via.placeholder.com/300", width=300, height=200),
+        ],
+        alignment=ft.MainAxisAlignment.START,
+        spacing=10,
+    )
